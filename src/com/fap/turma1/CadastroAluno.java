d04fe59b092928dbe8f93579253850dbb8c99f33
CeloDigital <celoartdigital@gmail.com>
2026-06-26 11:35:45 -0300

    Aula sobre métodos

--- src/com/fap/turma1/CadastroAluno.java
+++ src/com/fap/turma1/CadastroAluno.java
@@ -1,17 +1,37 @@
 package com.fap.turma1;
 
 public class CadastroAluno {
-
     String nome,email;
-
     int idade;
+    double desconto;
+    double mensalidade;
 
-    void listInfoAlunos() {
-        //METODO QUE GUARDA AS INFORMAÇÕES DOS ALUNOS
-        System.out.println(
-                "Nome do aluno: " + nome
-                + "\n" + "Email do aluno: " + email
-                + "\n" + "Idade do aluno: " + idade
-        );
+    void systemMessage() {
+
+        System.out.println("Seja bem-vindo ao sistema");
+        System.out.println(nome);
+        System.out.println(email);
+        System.out.println(idade);
     }
+
+    void exibirCurso(String curso) {
+        System.out.println("Curso: " + curso);
+    }
+
+    double calcularValorFinal() {
+        return mensalidade - desconto;
+    }
+
+    double calcularNotas(double nota1, double nota2, double nota3) {
+        return (nota1 + nota2 + nota3) / 3;
+    }
+
+    void verificarAprovacao(double media) {
+        if (media >= 7) {
+            System.out.println("ALUNO PROVADO!");
+        } else {
+            System.out.println("ALUNO REPROVADO!");
+        }
+    }
+
 }
--- src/com/fap/turma1/Main.java
+++ src/com/fap/turma1/Main.java
@@ -7,16 +7,22 @@ public class Main {
         aluno1.nome = "Marcelo";
         aluno1.email = "marcelo@hotmail.com";
         aluno1.idade = 28;
+        aluno1.mensalidade = 500;
+        aluno1.desconto = 150;
 
-        CadastroAluno aluno2 = new CadastroAluno();
-        aluno2.nome = "Marilia";
-        aluno2.email = "Marilia@hotmail.com";
-        aluno2.idade = 27;
-
-
-        aluno1.listInfoAlunos();
+        aluno1.systemMessage();
         System.out.println();
-        aluno2.listInfoAlunos();
+        aluno1.exibirCurso("Teste de Software");
+        System.out.println();
+
+        double valorFinal = aluno1.calcularValorFinal();
+        System.out.println(valorFinal);
+
+
+        double media = aluno1.calcularNotas(8, 7.3, 6);
+        System.out.printf("Média: %.1f ", media);
+
+        aluno1.verificarAprovacao(media);
     }
 
 }
